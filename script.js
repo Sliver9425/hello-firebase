f9acfc77f1cb36af6c2afeb920aeac0c53c084ca
Sliver9425 <damianalejandro.9422@gmail.com>
2025-11-09 19:24:30 -0500

    initial commit - hello-firebase

---
+++ script.js
@@ -0,0 +1,9 @@
+function showMessage() {
+  alert("Hello from Firebase Hosting! 🔥");
+  console.log("Firebase Hosting JavaScript is running correctly.");
+}
+
+// Solo para que se vea actividad en consola
+document.addEventListener("DOMContentLoaded", () => {
+  console.log("DOM fully loaded and parsed.");
+});
